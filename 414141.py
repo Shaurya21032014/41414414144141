0bff6fc097666f6fb08b801b9d95716af2920ee6
Shaurya21032014 <haritika.arora@gmail.com>
2026-02-08 15:27:26 +0530

    Add files via upload

---
+++ 414141.py
@@ -0,0 +1,16 @@
+def computepower(x,y):
+    result = 1
+
+    while ( y > 0):
+        if (y % 2 == 0):
+            x = x * x
+            y>>=1
+
+        else:
+            result = result * x
+            y = y -1
+        return result 
+    
+x = int(input("enter x for x^y"))
+y = int(input("enter x for x^y"))
+print("total", (computepower(x,y)))
